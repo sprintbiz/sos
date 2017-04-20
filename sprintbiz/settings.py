54cf378c17fbc82c2cb33b212dcc58b06e61ce24
sprintbiz <pawel.sawukajtys@gmail.com>
2017-04-20 20:13:07 +0200

    Clear database setup

--- sprintbiz/settings.py
+++ sprintbiz/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'sprintbiz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dsbsos',
+        'NAME': 'dsbsos_dev',
         'USER': 'root',
         'PASSWORD': 'oracle',
     }
